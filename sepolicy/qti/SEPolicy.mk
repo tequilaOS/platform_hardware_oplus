LineageOS/android_hardware_oplus

sepolicy: qti: Split sm8450 and sm8550 specific rules
Follows the new logic of split sepolicy_vndr repos because there
might be definitions set for a specific platform and not for the
other(s).

Change-Id: Icf37876a746907ecae07da73b4b11a0a7017cf4f

sepolicy/qti/SEPolicy.mk
@@ -4,19 +4,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/legacy-um)
-    SEPOLICY_PLATFORM := legacy-um
+    SEPOLICY_PLATFORM_COMMON := legacy-um
 else
-    SEPOLICY_PLATFORM := um
+    SEPOLICY_PLATFORM_COMMON := common-um
+    ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8450)
+        SEPOLICY_PLATFORM_SPECIFIC := sm8450
+    else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8550)
+        SEPOLICY_PLATFORM_SPECIFIC := sm8550
+    endif
 endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/vendor \
-    hardware/oplus/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
+    hardware/oplus/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM_COMMON)
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/private \
-    hardware/oplus/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
+    hardware/oplus/sepolicy/qti/private/$(SEPOLICY_PLATFORM_COMMON)
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public \
-    hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
+    hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM_COMMON)
+
+ifneq ($(SEPOLICY_PLATFORM_SPECIFIC),)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM_SPECIFIC)
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/private/$(SEPOLICY_PLATFORM_SPECIFIC)
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM_SPECIFIC)
+endif

tequilaOS/platform_hardware_oplus

sepolicy/qti/SEPolicy.mk
@@ -5,8 +5,10 @@
 #
 ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/legacy-um)
     SEPOLICY_PLATFORM := legacy-um
-else
-    SEPOLICY_PLATFORM := um
+else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8450)
+    SEPOLICY_PLATFORM := sm8450
+else ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr/sm8550)
+    SEPOLICY_PLATFORM := sm8550
 endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
@@ -20,3 +22,14 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public \
     hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
+
+ifneq ($(SEPOLICY_PLATFORM), legacy-um)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/vendor/common-um
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/private/common-um
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/public/common-um
+endif